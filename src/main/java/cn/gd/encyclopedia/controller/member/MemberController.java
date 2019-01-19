efc38c16bceda1d76e28ec3e5677770e4c5ece0a
ningjm <2719334283@qq.com>
2019-01-19 17:11:40 +0800

    添加会员API

---
+++ src/main/java/cn/gd/encyclopedia/controller/member/MemberController.java
@@ -0,0 +1,11 @@
+package cn.gd.encyclopedia.controller.member;
+
+/**
+ * @program: encyclopedia
+ * @description: 会员API
+ * @author: Mr.Ning
+ * @create: 2019-01-19 17:10
+ **/
+
+public class MemberController {
+}
