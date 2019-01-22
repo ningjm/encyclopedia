18d24316d0d006d3697f1f74c03a94c4c230d5e7
ningjm <2719334283@qq.com>
2019-01-22 18:23:48 +0800

    没改啥

--- src/main/java/cn/gd/encyclopedia/EncyclopediaApplication.java
+++ src/main/java/cn/gd/encyclopedia/EncyclopediaApplication.java
@@ -1,29 +1,27 @@
-package cn.gd.encyclopedia.controller.member;
+package cn.gd.encyclopedia;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 
 /**
  * @program: encyclopedia
- * @description: 会员API
+ * @description: 启动类
  * @author: Mr.Ning
- * @create: 2019-01-19 17:10
+ * @create: 2019-01-22 15:32
  **/
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EncyclopediaApplication {
 
-@RestController
-public class MemberController {
+    public static void main(String[] args) throws IOException {
 
-    @GetMapping("get_caipu")
-    public String getCaipu() throws IOException {
         URL u = new URL("http://route.showapi.com/1164-2?showapi_appid=85976&showapi_sign=0f5e472368a74be5bf2a15c584a71275");
         InputStream in = u.openStream();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -42,9 +40,13 @@ public class MemberController {
         String s = new String(b, "utf-8");
         Map map = JSON.parseObject(s, Map.class);
         Map showapi_res_body = (Map)map.get("showapi_res_body");
-        for (Object obj : map.values()){
-            System.out.println(obj.toString());
+        for (Object object : showapi_res_body.values()){
+            System.out.println(object.toString());
+//            for (Object map2 : ((Map)object).values()){
+//                System.out.println(map2.toString());
+//            }
         }
-        return s;
+
+//        SpringApplication.run(EncyclopediaApplication.class, args);
     }
 }
